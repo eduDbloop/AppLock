f74e5cea6047fd8db0bd6d32ec1746c392200c30
Eduardo Melgarejo <emelgarejo0411@gmail.com>
2013-03-29 20:28:35 +0100

    With Error on activity_main.xml and ERROR MESSAGE Conversion to Dalvik format failed: Unable to execute dex: Multiple dex file define Landroid/support/v4/app/Watson$OnCreate...

--- src/com/develop/hlapp/MainActivity.java
+++ src/com/develop/hlapp/MainActivity.java
@@ -1,22 +1,45 @@
 package com.develop.hlapp;
 
+import java.util.ArrayList;
+
+import android.content.Context;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+ 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.viewpagerindicator.PageIndicator;
+import com.viewpagerindicator.TitlePageIndicator;
+ 
 
-public class MainActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+public class MainActivity extends FragmentActivity {
+    
+	TestFragmentAdapter mAdapter;
+	ViewPager  mViewPager;
+	PageIndicator mIndicator;
+	
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
 		setContentView(R.layout.activity_main);
+		
+		mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
+		
+		mViewPager = (ViewPager)findViewById(R.id.pager);
+		mViewPager.setAdapter(mAdapter);
+		
+		mIndicator = (TitlePageIndicator)findViewById(R.id.indicator);
+		mIndicator.setViewPager(mViewPager);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+ 
 }
